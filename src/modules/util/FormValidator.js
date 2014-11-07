fddf6fce763216b4478dddbbede46e4e43412df5
mutongwu <shawnwu@qq.com>
2014-11-07 18:41:33 +0800

    1.添加Ajaxform插件、form验证多语言、日期选择css更新

--- src/modules/util/FormValidator.js
+++ src/modules/util/FormValidator.js
@@ -1,21 +1,50 @@
 
 var $ = require("lib/jquery");
+var Lang = require('lib/lang');
+
+var errorTxt = Lang.i18n('validator.errorTxt'),
+	numTxt = Lang.i18n('validator.numTxt'),
+	reqTxt = Lang.i18n('validator.required');
+
+
 var validator = {
-	clearInvalidFn: null,
-    invalidCls: 'error',
-    PHONE: /^1[358]\d{9}$/,
+	config: {
+		clearInvalidFn: null, //清除错误样式函数
+		
+		getLabelFn: null,	//定位label提示的函数
+		
+	    invalidCls: 'error' //错误样式
+	},
+	
+    PHONE: /^1[3578]\d{9}$/,
     NUM: /^\d+$/,
     BLANK: /^\s*$/,
     EMAIL: /^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]+$/,
     POSTCODE:  /^[1-9][0-9]{5}$/,
     IDCARD:/^((1[1-5])|(2[1-3])|(3[1-7])|(4[1-6])|(5[0-4])|(6[1-5])|71|(8[12])|91)\d{4}((19\d{2}(0[13-9]|1[012])(0[1-9]|[12]\d|30))|(19\d{2}(0[13578]|1[02])31)|(19\d{2}02(0[1-9]|1\d|2[0-8]))|(19([13579][26]|[2468][048]|0[48])0229))\d{3}(\d|X|x)?$/,
     PASSPORT: /^1[45][0-9]{7}|G[0-9]{8}|P[0-9]{7}|S[0-9]{7,8}|D[0-9]+$/,
+    
+    setConfig: function(args){
+    	$.extend(this.config,args);
+    },
+    _getLabel: function(el){
+    	if(this.config.getLabelFn){
+    		return  this.config.getLabelFn(el);
+    	}
+    	return el.attr("label") || el.closest('.field').children("label").text();
+    },
+    _clearInvalid: function(el){
+    	 this.config.clearInvalidFn ? this.config.clearInvalidFn(el):  el.removeClass(this.config.invalidCls);
+    },
     validate: function($form){
         var _this = this,pass = true,
             el = null, val = null,
             msg = '',
             rs = {};
+
         var fields = $form.get(0).querySelectorAll('input,select,textarea');
+
+        
         $.each(fields, function(i,item){
         	if(!item.name){
         		return;
@@ -23,51 +52,51 @@ var validator = {
             pass = false;
             el = $(item);
             val = item.value;
-            msg = el.attr("label") || el.closest('.field').children("label").text();
+            msg = _this._getLabel(el);
             msg = msg.replace(/^\*/,'').replace(/[:：]$/,'');
             vtype = el.attr("vtype");
             
             if(el.attr("required") && _this.BLANK.test(val)){
-                msg += "不能为空";
+                msg += reqTxt;
                 return pass;
             }else if(val){
                 var tmp = parseInt(el.attr("minLength")||0, 10);
                 if(tmp && val.length < tmp){
-                    msg += "不能少于"+ tmp + "位";
+                    msg += Lang.i18n('validator.minlength',tmp);
                     return pass;
                 }
                 tmp = parseInt(el.attr("maxLength")|| 0,10);
                 if(tmp > 0 && val.length > tmp){
-                    msg += "不能大于"+ tmp + "位";
+                    msg += Lang.i18n('validator.maxlength',tmp);
                     return pass;
                 }
                 
                 if(vtype === "phone" && !_this.PHONE.test(val)){
-                    msg = "请填写正确的"+ msg;
+                    msg = errorTxt + msg;
                     return pass;
                 }
                 if(vtype === "num" && !_this.NUM.test(val)){
-                    msg = "请填写数字的" + msg;
+                    msg = numTxt + msg;
                     return pass;
                 }
                 if(vtype === "email" && !_this.EMAIL.test(val)){
-                    msg = "请填写正确的" + msg;
+                    msg = errorTxt + msg;
                     return pass;
                 }
                 if(vtype === "postcode" && !_this.POSTCODE.test(val)){
-                    msg = "请填写正确的" + msg;
+                    msg = errorTxt + msg;
                     return pass;
                 }
                 if(vtype === "idcard" && !_this.IDCARD.test(val)){
-                    msg = "请填写正确的" + msg;
+                    msg = errorTxt + msg;
                     return pass;
                 }
                 if(vtype === "passport" && !_this.PASSPORT.test(val)){
-                    msg = "请填写正确的" + msg;
+                    msg = errorTxt + msg;
                     return pass;
                 }
             }
-            _this.clearInvalidFn ? _this.clearInvalidFn(el):  el.removeClass(_this.invalidCls);
+            _this._clearInvalid(el);
             pass = true;
             if(item.type && item.type.toLowerCase() === 'radio'){
                 if(item.checked){
